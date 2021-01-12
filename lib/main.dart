21b0b965b21d222d20a9b14578833943ba8325dd
Hoang <hoanglxph09371@fpt.edu.vn>
2021-01-12 15:30:24 +0700

    push app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flash_chat_app/screens/chat_screen.dart';
+import 'package:flash_chat_app/screens/login_screen.dart';
+import 'package:flash_chat_app/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'screens/welcome_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.route,
+      routes: {
+        WelcomeScreen.route: (context) => WelcomeScreen(),
+        RegistrationScreen.route: (context) => RegistrationScreen(),
+        LoginScreen.route: (context) => LoginScreen(),
+        ChatScreen.route: (context) => ChatScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flash_chat_app/components/rounded_button.dart';
+import 'package:flash_chat_app/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+import 'login_screen.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const route = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController aniController;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    aniController = AnimationController(
+      duration: Duration(
+        seconds: 1,
+      ),
+      vsync: this,
+    );
+
+    // animation = CurvedAnimation(
+    //   parent: aniController,
+    //   curve: Curves.easeIn,
+    // );
+
+    animation =
+        ColorTween(begin: Colors.red, end: Colors.blue).animate(aniController);
+
+    //aniController.forward();
+    aniController.forward();
+
+    aniController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                TypewriterAnimatedTextKit(
+                  text: ['Flash Chat'],
+                  textStyle: TextStyle(
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+              color: Colors.lightBlueAccent,
+              title: 'Log In',
+              onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.route);
+              },
+            ),
+            RoundedButton(
+              color: Colors.blueAccent,
+              title: 'Register',
+              onPressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.route);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
